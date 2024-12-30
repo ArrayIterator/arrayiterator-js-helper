56d476e39ed9f612afbe0fe9cf1bcaff33cdca02
ArrayIterator <37867551+ArrayIterator@users.noreply.github.com>
2024-12-30 14:44:08 +0700

    Update Helper.js

--- Helper.js
+++ Helper.js
@@ -902,33 +902,41 @@ export const base64_encode = (string) => {
         return '';
     }
 
-    let result = '';
-    let i = 0;
-    do {
-        let a = string.charCodeAt(i++);
-        let b = string.charCodeAt(i++);
-        let c = string.charCodeAt(i++);
+    
+    // From https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem.
+    const bytes = new Uint8Array(string.length);
+    string.split('').forEach((v, i) => {
+        bytes[i] = v.charCodeAt(0);
+    });
+    const binString = String.fromCodePoint(...bytes);
+    return btoa(binString);
+    // let result = '';
+    // let i = 0;
+    // do {
+    //     let a = string.charCodeAt(i++);
+    //     let b = string.charCodeAt(i++);
+    //     let c = string.charCodeAt(i++);
 
-        a = a ? a : 0;
-        b = b ? b : 0;
-        c = c ? c : 0;
+    //     a = a ? a : 0;
+    //     b = b ? b : 0;
+    //     c = c ? c : 0;
 
-        const b1 = (a >> 2) & 0x3F;
-        const b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
-        let b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
-        let b4 = c & 0x3F;
+    //     const b1 = (a >> 2) & 0x3F;
+    //     const b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
+    //     let b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
+    //     let b4 = c & 0x3F;
 
-        if (!b) {
-            b3 = b4 = 64;
-        } else if (!c) {
-            b4 = 64;
-        }
+    //     if (!b) {
+    //         b3 = b4 = 64;
+    //     } else if (!c) {
+    //         b4 = 64;
+    //     }
 
-        result += Base64Chars.charAt(b1) + Base64Chars.charAt(b2) + Base64Chars.charAt(b3) + Base64Chars.charAt(b4);
+    //     result += Base64Chars.charAt(b1) + Base64Chars.charAt(b2) + Base64Chars.charAt(b3) + Base64Chars.charAt(b4);
 
-    } while (i < string.length);
+    // } while (i < string.length);
 
-    return result;
+    // return result;
 }
 
 /**
@@ -940,25 +948,31 @@ export const base64_decode = (string) => {
     if (!is_string(string)) {
         return '';
     }
+    // From https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem.
+    const binString = atob(string);
+    const buffer = new Uint8Array(binString.length);
+    binString.split('').forEach((v, i) => {
+        buffer[i] = v.charCodeAt(0);
+    });
+    return String.fromCharCode(...buffer);
+    // let result = '';
 
-    let result = '';
+    // let i = 0;
+    // do {
+    //     const b1 = Base64Chars.indexOf(string.charAt(i++));
+    //     const b2 = Base64Chars.indexOf(string.charAt(i++));
+    //     const b3 = Base64Chars.indexOf(string.charAt(i++));
+    //     const b4 = Base64Chars.indexOf(string.charAt(i++));
 
-    let i = 0;
-    do {
-        const b1 = Base64Chars.indexOf(string.charAt(i++));
-        const b2 = Base64Chars.indexOf(string.charAt(i++));
-        const b3 = Base64Chars.indexOf(string.charAt(i++));
-        const b4 = Base64Chars.indexOf(string.charAt(i++));
+    //     const a = ((b1 & 0x3F) << 2) | ((b2 >> 4) & 0x3);
+    //     const b = ((b2 & 0xF) << 4) | ((b3 >> 2) & 0xF);
+    //     const c = ((b3 & 0x3) << 6) | (b4 & 0x3F);
 
-        const a = ((b1 & 0x3F) << 2) | ((b2 >> 4) & 0x3);
-        const b = ((b2 & 0xF) << 4) | ((b3 >> 2) & 0xF);
-        const c = ((b3 & 0x3) << 6) | (b4 & 0x3F);
+    //     result += String.fromCharCode(a) + (b ? String.fromCharCode(b) : '') + (c ? String.fromCharCode(c) : '');
 
-        result += String.fromCharCode(a) + (b ? String.fromCharCode(b) : '') + (c ? String.fromCharCode(c) : '');
+    // } while (i < string.length);
 
-    } while (i < string.length);
-
-    return result;
+    // return result;
 }
 /**
  * Encode base32 string
